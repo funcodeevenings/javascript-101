f7fd4d6ae07e814c450294cdc86a0cfb3fd98e9e
narutoadi <mis.ad1206@gmail.com>
2020-12-10 22:25:52 +0530

    First commit

--- Session-2/script2.js
+++ Session-2/script2.js
@@ -1,6 +1,29 @@
+ram=23;
+shyam = 23;
+
+if(ram > shyam){
+	console.log("Ram bada h");
+}else if(ram==shyam){
+	console.log("Both are of equal age.")
+}
+else{
+	console.log("Shyam is elder.");
+}
+
+console.log("outside if statement");
+
+// conditional operaters 
+//  ==, >, <, >=, <= 
+
+
+// Logical operators
+// &&, ||, !
+
+
 // Link this script to index.html
 
 // Assign ages to Ram and Shyam and print name of elder brother.
+
 
 
 // Assign number to a variable and print if it is even or odd.
@@ -16,7 +39,6 @@
 
 
 // Given 3 angles of a triangle print if it is valid or not.
-
 
 
 // Given 2 angles of a triangle print value of third angle
