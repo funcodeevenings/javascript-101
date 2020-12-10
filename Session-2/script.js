53f48f6402534142a1461b4a1a49202776713b0e
Aditi Mishra <adimishr@192.168.0.104>
2020-12-10 20:53:51 +0530

    Session 2 assignments updated

---
+++ Session-2/script.js
@@ -0,0 +1,30 @@
+console.log("Session 2");
+p1 = document.getElementById("p1");
+p1.innerHTML="updated p1"
+
+p2 = document.getElementById("p2");
+p2.innerHTML="updated p2"
+
+p3 = document.getElementById("p3");
+p3.innerHTML="updated p3"
+
+// Datatypes - boolean, numbers, strings, objects
+
+
+// Arithmatic operators +, -, *, /, **, %, ++, --
+
+// Assignment-1
+
+
+// Assignment-2
+
+
+// Assignment-3
+
+
+// Assignment-4
+
+
+// Assignment-5
+
+
---
+++ Session-2/script2.js
@@ -0,0 +1,22 @@
+// Link this script to index.html
+
+// Assign ages to Ram and Shyam and print name of elder brother.
+
+
+// Assign number to a variable and print if it is even or odd.
+
+
+// Print maximum of 2 numbers
+
+
+// Print maximum of 3 numbers
+
+
+// Check if a character is vowel or consonant
+
+
+// Given 3 angles of a triangle print if it is valid or not.
+
+
+
+// Given 2 angles of a triangle print value of third angle
